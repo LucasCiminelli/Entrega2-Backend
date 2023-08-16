03d0d2ec2c3c0fa917ba1066656981bdde3d6016
Lucas Ciminelli <lucasaciminelli@gmail.com>
2023-08-16 00:31:15 -0300

    productManager

--- ProductManager.js
+++ ProductManager.js
@@ -9,18 +9,20 @@ class ProductManager {
     this.path = "./products.json";
 
     if (fs.existsSync(this.path)) {
-        const content = fs.readFileSync(this.path, "utf-8");
-        const products = JSON.parse(content);
-        if (products.length > 0) {
-          ProductManager.id = products[products.length - 1].id + 1;
-        }
+      const content = fs.readFileSync(this.path, "utf-8");
+      const products = JSON.parse(content);
+      if (products.length > 0) {
+        ProductManager.id = products[products.length - 1].id + 1;
       }
+    }
   }
+  
 
   async getProducts() {
     const data = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
     return data;
   }
+
 
   async addProduct(title, description, price, thumbnail, code, stock) {
     const product = {
@@ -34,76 +36,93 @@ class ProductManager {
     };
 
     try {
-        if (!fs.existsSync(this.path)) {
-          const arrayProducts = [];
-          arrayProducts.push(product);
-      
-          await fs.promises.writeFile(
-            this.path,
-            JSON.stringify(arrayProducts, null, "\t")
-          );
-        } else {
-          const content = await this.getProducts();
-          console.log("File content:", content);
-      
-          content.push(product);
-          await fs.promises.writeFile(
-            this.path,
-            JSON.stringify(content, null, "\t")
-          );
-      
-          ProductManager.id++; // Incrementar el ID después de actualizar el archivo
-        }
-      } catch (error) {
-        console.log(error);
+      if (!fs.existsSync(this.path)) {
+        const arrayProducts = [];
+        arrayProducts.push(product);
+
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(arrayProducts, null, "\t")
+        );
+      } else {
+        const content = await this.getProducts();
+        console.log("File content:", content);
+
+        content.push(product);
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(content, null, "\t")
+        );
+
+        ProductManager.id++;
       }
-      
-
-    // if (
-    //   product.title === undefined ||
-    //   product.id === undefined ||
-    //   product.description === undefined ||
-    //   product.price === undefined ||
-    //   product.thumbnail === undefined ||
-    //   product.code === undefined ||
-    //   product.stock === undefined
-    // ) {
-    //   console.error("Faltan completar campos");
-    //   return;
-    // }
-
-    //  this.products.push(product);
-    //  ProductManager.id++;
-    // return product;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  //   async getProducts() {
-  //     const data = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
-  //     return data;
-  //   }
 
-  //   getProductsById(id) {
-  //     const foundProduct = this.products.find((prod) => prod.id === id);
-  //     if (!foundProduct) {
-  //       console.error(
-  //         "Error, el ID ingresado no coincide con ningun producto. Ingrese un ID válido"
-  //       );
-  //       return;
-  //     }
-  //     return console.log(
-  //       "El id ingresado corresponde al siguiente producto: ",
-  //       foundProduct
-  //     );
-  //   }
+  async getProductsById(id) {
+    const data = await this.getProducts();
+    const foundProduct = data.find((prod) => prod.id === id);
+    if (!foundProduct) {
+      console.error(
+        "Error, el ID ingresado no coincide con ningun producto. Ingrese un ID válido"
+      );
+      return;
+    }
+    return foundProduct;
+  }
 
-  //   getProductRepetido(code) {
-  //     const repetido = this.products.find((prod) => prod.code === code);
-  //     if (repetido) {
-  //       console.log("Producto repetido", repetido);
-  //       return repetido;
-  //     }
-  //   }
+
+  async deleteProduct(id) {
+    const data = await this.getProducts();
+    const findDelete = data.findIndex((prod) => prod.id === id);
+    if (findDelete !== -1) {
+      data.splice(findDelete, 1);
+
+      try {
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(data, null, "\t")
+        );
+        console.log(`Producto con id: ${id} eliminado.`);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log("No se encontro el producto que se desea eliminar");
+    }
+  }
+
+
+  async updateProduct(id, productChanged) {
+    const data = await this.getProducts();
+    const findProduct = data.find((prod) => prod.id === id);
+
+    if (findProduct) {
+      Object.assign(findProduct, productChanged);
+      productChanged.id = id;
+      await fs.promises.writeFile(this.path, JSON.stringify(data, null, "\t"));
+      return findProduct;
+    } else {
+      console.log(error);
+      return null;
+    }
+  }
+
+
+  async getProductRepetido(code) {
+    const data = await this.getProducts();
+    const repetido = data.find((prod) => prod.code === code);
+    if (repetido) {
+      console.log("Producto repetido", repetido);
+      return repetido;
+    }
+    return undefined;
+  }
 }
+
 
 const asyncFunction = async () => {
   const manager = new ProductManager();
@@ -119,19 +138,3 @@ const asyncFunction = async () => {
 };
 
 asyncFunction();
-
-// manager.addProduct(
-//   "producto prueba2",
-//   "Este es un producto prueba2",
-//   200,
-//   "Sin imagen2",
-//   "abc123",
-//   10
-// );
-
-// const verProducts = manager.getProducts();
-// console.log("Products:", verProducts);
-
-// const productoBuscado = manager.getProductsById(4);
-
-// const productoRepetido = manager.getProductRepetido("abc123");
